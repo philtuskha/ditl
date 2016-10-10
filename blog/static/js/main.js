3e2bb5c981e2138c91cbba398e5400f65c87d03a
eyas <phil.tuskha@gmail.com>
2016-10-09 22:04:34 -0400

    re-layout part5

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -217,10 +217,10 @@ $(document).ready(function() {
 
 								
 									setTimeout(function(){
-										$("body").css({"margin-top":$(this).scrollTop()+"px"})
+										$("body").css({transition:"margin-top 0.5s", "margin-top":$(this).scrollTop()+"px"})
 										$( "body" ).one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
 											 $(window).scrollTop(0)
-											$("body").css({"margin-top":"0px"})
+											$("body").css({transition: "none !important", "margin-top":"0px"})
 											$("body").removeAttr("style")
 											console.log($("body").attr("style"))
 											//$("body").off('touchend')
