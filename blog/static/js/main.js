8db2f43b3cd1e36f4a1f476e6a058a1667566589
eyas <phil.tuskha@gmail.com>
2016-10-10 00:28:21 -0400

    re-layout part5

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -239,13 +239,16 @@ $(document).ready(function() {
 										
 										if(e.target.id != "id_text"){
 										
-											$("body").css({transition:"margin-top 0.5s", "margin-top":"-"+$(this).scrollTop()+"px"})
+											$("body").css({"margin-top":"-"+$(this).scrollTop()+"px"})
+											
 											
 											 setTimeout(function(){
+												$("body").css({transition:"margin-top 0.7s", "margin-top":"0px"})
+												myScroll(el, $(this).scrollTop());
 												
 												$( "body" ).one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
 													 $(window).scrollTop(0)
-													$("body").css({transition: "none !important", "margin-top":"0px"})
+													$("body").css({transition: "none !important"})
 													$("body").removeAttr("style")
 													
 													el.css({overflow:"scroll"})
