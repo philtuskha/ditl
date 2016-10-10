8b6d91b205416d883f7895d24b14a08f66b23f6e
eyas <phil.tuskha@gmail.com>
2016-10-10 11:56:40 -0400

    re-layout part5

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -347,7 +347,6 @@ $(document).ready(function() {
 											el.animate({ scrollTop: scroll_dist}, 1000, 'easeOutQuint', function initElScroll(e){
 												//console.log(e)
 												//$(window).scrollTop(0)
-												e.stopPropagation();
 												el.off("scroll", initElScroll);
 												//$(window).scrollTop(0)
 												$("html, body").animate({ scrollTop: 0}, 500, 'easeOutQuint', function initWindowScroll(){
