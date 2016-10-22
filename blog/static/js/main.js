6ab4a8daf4127030f2a56c660c093e1ce88888d7
eyas <phil.tuskha@gmail.com>
2016-10-22 03:00:09 -0400

    mobile checks

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1788,19 +1788,19 @@ function hiddenToggleFunction(){
 			var startY = 0
 			var startTime = 0
 			
-			$(window).on("touchstart", function(e){
-				e.stopPropagation();
-				
-				startY = e.originalEvent.changedTouches[0].pageY
-				startTime = e.originalEvent.timeStamp
-				if($(this).scrollTop() != 1){
-					$('.user-view').css({overflow:'hidden'})
-				
-				}
-				
-				
-				console.log("TOUchsTart!!", e, $(this).scrollTop())
-			});	
+			// $(window).on("touchstart", function(e){
+// 				e.stopPropagation();
+// 				
+// 				startY = e.originalEvent.changedTouches[0].pageY
+// 				startTime = e.originalEvent.timeStamp
+// 				if($(this).scrollTop() != 1){
+// 					$('.user-view').css({overflow:'hidden'})
+// 				
+// 				}
+// 				
+// 				
+// 				console.log("TOUchsTart!!", e, $(this).scrollTop())
+// 			});	
 			
 			$(window).on("touchend", function(e){	
 				console.log("TOUCH END!!!!!!!!!", e, $(this).scrollTop())
