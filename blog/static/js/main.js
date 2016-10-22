c0d403b33f29e5307af1bb43ae3f745639baf511
eyas <phil.tuskha@gmail.com>
2016-10-22 03:59:48 -0400

    mobile checks

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1782,11 +1782,8 @@ function hiddenToggleFunction(){
 	
 	
 	var Mobile = (function(){
-		var t_start = 0,
-			t_end = 0,
-			t_diff = t_start - t_end;
 			
-		var _header = function(){
+		var _header = function(t_diff){
 			console.log(t_diff)
 			var header = $("header")
 			
@@ -1823,46 +1820,50 @@ function hiddenToggleFunction(){
 		}
 		
 		var _start = function(e){
-			var el = _getElement(e);
 			
-			t_start = e.originalEvent.changedTouches[0].pageY
-			
-			if(el){
-			
-				if($(this).scrollTop() != 1){
-					el.css({overflow:'hidden'})
-				}
-			}
 		}
 		
 		
 		var _end = function(e){
-			var el = _getElement(e)
 			
-			t_end = e.originalEvent.changedTouches[0].pageY
-						
-			if(el){
-			
-				if($(this).scrollTop() <= 0){
-					el.css({overflow:'hidden'})	
-				}else{
-					$("html, body").animate({ scrollTop: 1}, 200, 'easeOutQuint', function initWindowScroll(){
-						$("html, body").off("scroll", initWindowScroll);
-						el.css({overflow:'scroll'})
-					});
-				}
-				
-				_header();
-			}
 		}
 			
 		var _bindTouchEvents = function(){
+			var t_start = 0,
+			t_end = 0,
+			t_diff = t_start - t_end;
+			
 			$(window).on("touchstart", function(e){
-				_start(e);	
-			});
+				var el = _getElement(e);
+			
+				t_start = e.originalEvent.changedTouches[0].pageY
+			
+				if(el){
+			
+					if($(this).scrollTop() != 1){
+						el.css({overflow:'hidden'})
+					}
+				}	
+				});
 			
 			$(window).on("touchend", function(e){
-				_end(e);
+				var el = _getElement(e)
+			
+				t_end = e.originalEvent.changedTouches[0].pageY
+						
+				if(el){
+			
+					if($(this).scrollTop() <= 0){
+						el.css({overflow:'hidden'})	
+					}else{
+						$("html, body").animate({ scrollTop: 1}, 200, 'easeOutQuint', function initWindowScroll(){
+							$("html, body").off("scroll", initWindowScroll);
+							el.css({overflow:'scroll'})
+						});
+					}
+				
+					_header(t_diff);
+				}
 			});
 		}
 		
