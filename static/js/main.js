822c88a98243db802f6690dc0e4ece84d5484a88
eyas <phil.tuskha@gmail.com>
2016-11-09 15:49:41 -0500

    removed '/static from .gitignore' included admin static files in main directory

---
+++ static/js/main.js
@@ -0,0 +1,53 @@
+ alert("pop")
+  // AJAX POST
+    $('.add-todo').click(function(){
+      console.log('am i called');
+
+        $.ajax({
+            type: "POST",
+            url: "/ajax/add/",
+            dataType: "json",
+            data: { "item": $(".todo-item").val() },
+            success: function(data) {
+                alert(data.message);
+            }
+        });
+
+    });
+
+
+
+    // CSRF code
+    function getCookie(name) {
+        var cookieValue = null;
+        var i = 0;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (i; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    }); 
+
+
+});
