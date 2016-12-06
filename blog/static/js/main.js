7b7f8962412d38a293f05c86e0b36093933def6c
eyas <phil.tuskha@gmail.com>
2016-12-06 14:53:40 -0500

    oneThread pop up orientation fix

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -55,11 +55,11 @@ $(document).ready(function() {
 			
 			if(center_check == 1 && 'ontouchstart' in document.documentElement){
 				$("#id_text_r").on('focus', function(){
-					$('#thread-div-pop').css({top:"116px"})
+					$('#thread-div-pop').css({bottom:"0px"}) // top:"116px"
 					$('#thread-wrapper-pop').css({background:"rgba(248,248,248,1)", height:"150vh"});
 				
 				}).on('blur', function(){
-					$('#thread-div-pop').css({top:"0px"})
+					$('#thread-div-pop').css({bottom:""}) //top:"0px"
 					$('#thread-wrapper-pop').css({background:"rgba(0,0,0,0.1)", height:"100vh"});
 				});
 				
