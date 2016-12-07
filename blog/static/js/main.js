9e2f6ef01c5ba0f497273c93f34707b81661e9a3
eyas <phil.tuskha@gmail.com>
2016-12-06 23:20:31 -0500

    virtual keyboard onfocus fixes

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 			}, 30);
 			
 			if('ontouchstart' in document.documentElement){
-				$(document).on('mousemove', function(e){
+				$(document).on('touchmove', function(e){
 					e.stopPropagation();
 					console.log(e);
 					//$(this)scrollTop($(this)scrollTop() += )
