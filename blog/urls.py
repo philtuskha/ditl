33e38453f00916dd5bbaced83173731137ae0e9b
pantsPantskinskins <pantskins@pantskinss-Mini.attlocal.net>
2020-02-21 11:11:56 -0500

    update urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,22 @@
 from . import views
 #from . import ajax
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^thread_list$', views.thread_list, name='thread_list'),
-    url(r'^status/$', views.status, name='status'),
-    url(r'^notifications/$', views.notifications, name='notifications'),
-    url(r'^user_view/$', views.user_view, name='user_view'),
-    url(r'^keywords$', views.keywords, name='keywords'),
-    url(r'^thread/(?P<pk>[0-9]+)/$', views.thread_detail, name='thread_detail'),
-    url(r'^thread_detail_detail/(?P<pk>[0-9]+)/$', views.thread_detail_detail, name='thread_detail_detail'),
-    url(r'^vote/$', views.vote, name='vote'),
-    url(r'^update_page/$', views.update_page, name='update_page'),
-    url(r'^delete_post/$', views.delete_post, name='delete_post'),
-    url(r'^restore_post/$', views.restore_post, name='restore_post'),
-    url(r'^thread/new/$', views.thread_new, name='thread_new'),
-    url(r'^$', views.home, name='home'),
-    url(r'^ajax/add/$', views.add_thread, name='add_thread'),
-    url(r'^profile_test/$', views.profile_test, name='profile_test'),
-    url(r'^guest_user/$', views.guest_user, name='guest_user'),
+    path('thread_list', views.thread_list, name='thread_list'),
+    path('status/', views.status, name='status'),
+    path('notifications/', views.notifications, name='notifications'),
+    path('user_view/', views.user_view, name='user_view'),
+    path('keywords', views.keywords, name='keywords'),
+    path('thread/(?P<pk>[0-9]+)/', views.thread_detail, name='thread_detail'),
+    path('thread_detail_detail/(?P<pk>[0-9]+)/', views.thread_detail_detail, name='thread_detail_detail'),
+    path('vote/', views.vote, name='vote'),
+    path('update_page/', views.update_page, name='update_page'),
+    path('delete_post/', views.delete_post, name='delete_post'),
+    path('restore_post/', views.restore_post, name='restore_post'),
+    path('thread/new/', views.thread_new, name='thread_new'),
+    path('', views.home, name='home'),
+    path('ajax/add/', views.add_thread, name='add_thread'),
+    path('profile_test/', views.profile_test, name='profile_test'),
+    path('guest_user/', views.guest_user, name='guest_user'),
 ]
