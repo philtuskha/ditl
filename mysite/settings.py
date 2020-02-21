c7084f8ca45038fa219faf3498042d42bc772b07
pantsPantskinskins <pantskins@pantskinss-Mini.attlocal.net>
2020-02-21 13:51:24 -0500

    typo installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'social.apps.django_app.default',
+    'social_django',
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = (
